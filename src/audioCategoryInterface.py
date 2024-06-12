663747200450d35c976e83d262cab21c6066db48
Fabian-Vega <fabianvega15502@gmail.com>
2024-06-11 20:32:22 -0600

    Added initial classifier code

---
+++ src/audioCategoryInterface.py
@@ -0,0 +1,78 @@
+import os
+import random
+import shutil
+import tkinter as tk
+from tkinter import messagebox
+from pydub import AudioSegment
+from pydub.playback import play
+import simpleaudio as sa
+
+# Paths
+unheard_folder = '../data/Marlon'
+sirens_folder = '../data/Marlon/sirens'
+ambient_folder = '../data/Marlon/ambient'
+unusable_folder = '../data/Marlon/unusable'
+
+# Function to play audio
+def play_audio(file_path):
+    sound = AudioSegment.from_file(file_path)
+    play(sound)
+
+# Function to move file to the specified folder
+def move_file(file_path, target_folder):
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    shutil.move(file_path, target_folder)
+
+# Function to load and play a random unheard audio file
+def load_random_audio():
+    unheard_files = [f for f in os.listdir(unheard_folder) if f.endswith('.mp3')]
+    if not unheard_files:
+        messagebox.showinfo("Info", "No more unheard audio files!")
+        return None
+    random_file = random.choice(unheard_files)
+    return os.path.join(unheard_folder, random_file)
+
+# Callback functions for buttons
+def classify_as_siren():
+    global current_file
+    if current_file:
+        move_file(current_file, sirens_folder)
+        next_audio()
+
+def classify_as_ambient():
+    global current_file
+    if current_file:
+        move_file(current_file, ambient_folder)
+        next_audio()
+
+def classify_as_unusable():
+    global current_file
+    if current_file:
+        move_file(current_file, unusable_folder)
+        next_audio()
+
+def next_audio():
+    global current_file
+    current_file = load_random_audio()
+    if current_file:
+        play_audio(current_file)
+
+# Setting up the UI
+root = tk.Tk()
+root.title("Audio Classifier")
+
+current_file = None
+
+btn_siren = tk.Button(root, text="Siren", command=classify_as_siren, width=25, height=5)
+btn_siren.pack(pady=10)
+
+btn_ambient = tk.Button(root, text="Ambient Noise", command=classify_as_ambient, width=25, height=5)
+btn_ambient.pack(pady=10)
+
+btn_unusable = tk.Button(root, text="Unusable", command=classify_as_unusable, width=25, height=5)
+btn_unusable.pack(pady=10)
+
+next_audio()
+
+root.mainloop()
--- src/audioClassifier.py
+++ src/audioClassifier.py
@@ -1,78 +1,185 @@
 import os
-import random
-import shutil
-import tkinter as tk
-from tkinter import messagebox
-from pydub import AudioSegment
-from pydub.playback import play
-import simpleaudio as sa
+from matplotlib import pyplot as plt
+import tensorflow as tf 
+import tensorflow_io as tfio
 
-# Paths
-unheard_folder = '../data/Marlon'
-sirens_folder = '../data/Marlon/sirens'
-ambient_folder = '../data/Marlon/ambient'
-unusable_folder = '../data/Marlon/unusable'
+#pip install tensorflow==2.4.1 tensorflow-gpu==2.4.1 tensorflow-io matplotlib
+CAPUCHIN_FILE = os.path.join('data', 'Parsed_Capuchinbird_Clips', 'XC3776-3.wav')
+NOT_CAPUCHIN_FILE = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips', 'afternoon-birds-song-in-forest-0.wav')
 
-# Function to play audio
-def play_audio(file_path):
-    sound = AudioSegment.from_file(file_path)
-    play(sound)
 
-# Function to move file to the specified folder
-def move_file(file_path, target_folder):
-    if not os.path.exists(target_folder):
-        os.makedirs(target_folder)
-    shutil.move(file_path, target_folder)
+def load_wav_16k_mono(filename):
+    # Load encoded wav file
+    file_contents = tf.io.read_file(filename)
+    # Decode wav (tensors by channels) 
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    # Removes trailing axis
+    wav = tf.squeeze(wav, axis=-1)
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Goes from 44100Hz to 16000hz - amplitude of the audio signal
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+    return wav
 
-# Function to load and play a random unheard audio file
-def load_random_audio():
-    unheard_files = [f for f in os.listdir(unheard_folder) if f.endswith('.mp3')]
-    if not unheard_files:
-        messagebox.showinfo("Info", "No more unheard audio files!")
-        return None
-    random_file = random.choice(unheard_files)
-    return os.path.join(unheard_folder, random_file)
 
-# Callback functions for buttons
-def classify_as_siren():
-    global current_file
-    if current_file:
-        move_file(current_file, sirens_folder)
-        next_audio()
+wave = load_wav_16k_mono(CAPUCHIN_FILE)
+nwave = load_wav_16k_mono(NOT_CAPUCHIN_FILE)
+plt.plot(wave)
+plt.plot(nwave)
+plt.show()
 
-def classify_as_ambient():
-    global current_file
-    if current_file:
-        move_file(current_file, ambient_folder)
-        next_audio()
 
-def classify_as_unusable():
-    global current_file
-    if current_file:
-        move_file(current_file, unusable_folder)
-        next_audio()
+POS = os.path.join('data', 'Parsed_Capuchinbird_Clips')
+NEG = os.path.join('data', 'Parsed_Not_Capuchinbird_Clips')
 
-def next_audio():
-    global current_file
-    current_file = load_random_audio()
-    if current_file:
-        play_audio(current_file)
 
-# Setting up the UI
-root = tk.Tk()
-root.title("Audio Classifier")
+pos = tf.data.Dataset.list_files(POS+'\*.wav')
+neg = tf.data.Dataset.list_files(NEG+'\*.wav')
 
-current_file = None
 
-btn_siren = tk.Button(root, text="Siren", command=classify_as_siren, width=25, height=5)
-btn_siren.pack(pady=10)
+positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
+negatives = tf.data.Dataset.zip((neg, tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
+data = positives.concatenate(negatives)
 
-btn_ambient = tk.Button(root, text="Ambient Noise", command=classify_as_ambient, width=25, height=5)
-btn_ambient.pack(pady=10)
 
-btn_unusable = tk.Button(root, text="Unusable", command=classify_as_unusable, width=25, height=5)
-btn_unusable.pack(pady=10)
+lengths = []
+for file in os.listdir(os.path.join('data', 'Parsed_Capuchinbird_Clips')):
+    tensor_wave = load_wav_16k_mono(os.path.join('data', 'Parsed_Capuchinbird_Clips', file))
+    lengths.append(len(tensor_wave))
+    
+tf.math.reduce_mean(lengths)
+tf.math.reduce_min(lengths)
+tf.math.reduce_max(lengths)
 
-next_audio()
+def preprocess(file_path, label): 
+    wav = load_wav_16k_mono(file_path)
+    wav = wav[:48000]
+    zero_padding = tf.zeros([48000] - tf.shape(wav), dtype=tf.float32)
+    wav = tf.concat([zero_padding, wav],0)
+    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=32)
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.expand_dims(spectrogram, axis=2)
+    return spectrogram, label
 
-root.mainloop()
+filepath, label = positives.shuffle(buffer_size=10000).as_numpy_iterator().next()
+spectrogram, label = preprocess(filepath, label)
+plt.figure(figsize=(30,20))
+plt.imshow(tf.transpose(spectrogram)[0])
+plt.show()
+
+
+data = data.map(preprocess)
+data = data.cache()
+data = data.shuffle(buffer_size=1000)
+data = data.batch(16)
+data = data.prefetch(8)
+
+
+train = data.take(36)
+test = data.skip(36).take(15)
+
+samples, labels = train.as_numpy_iterator().next()
+samples.shape
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+
+model = Sequential()
+model.add(Conv2D(16, (3,3), activation='relu', input_shape=(1491, 257,1)))
+model.add(Conv2D(16, (3,3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile('Adam', loss='BinaryCrossentropy', metrics=[tf.keras.metrics.Recall(),tf.keras.metrics.Precision()])
+model.summary()
+
+hist = model.fit(train, epochs=4, validation_data=test)
+plt.title('Loss')
+plt.plot(hist.history['loss'], 'r')
+plt.plot(hist.history['val_loss'], 'b')
+plt.show()
+plt.title('Precision')
+plt.plot(hist.history['precision'], 'r')
+plt.plot(hist.history['val_precision'], 'b')
+plt.show()
+plt.title('Recall')
+plt.plot(hist.history['recall'], 'r')
+plt.plot(hist.history['val_recall'], 'b')
+plt.show()
+
+X_test, y_test = test.as_numpy_iterator().next()
+yhat = model.predict(X_test)
+
+
+yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
+
+def load_mp3_16k_mono(filename):
+    """ Load a WAV file, convert it to a float tensor, resample to 16 kHz single-channel audio. """
+    res = tfio.audio.AudioIOTensor(filename)
+    # Convert to tensor and combine channels 
+    tensor = res.to_tensor()
+    tensor = tf.math.reduce_sum(tensor, axis=1) / 2 
+    # Extract sample rate and cast
+    sample_rate = res.rate
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Resample to 16 kHz
+    wav = tfio.audio.resample(tensor, rate_in=sample_rate, rate_out=16000)
+    return wav
+mp3 = os.path.join('data', 'Forest Recordings', 'recording_00.mp3')
+wav = load_mp3_16k_mono(mp3)
+audio_slices = tf.keras.utils.timeseries_dataset_from_array(wav, wav, sequence_length=48000, sequence_stride=48000, batch_size=1)
+samples, index = audio_slices.as_numpy_iterator().next()
+
+def preprocess_mp3(sample, index):
+    sample = sample[0]
+    zero_padding = tf.zeros([48000] - tf.shape(sample), dtype=tf.float32)
+    wav = tf.concat([zero_padding, sample],0)
+    spectrogram = tf.signal.stft(wav, frame_length=320, frame_step=32)
+    spectrogram = tf.abs(spectrogram)
+    spectrogram = tf.expand_dims(spectrogram, axis=2)
+    return spectrogram
+
+audio_slices = tf.keras.utils.timeseries_dataset_from_array(wav, wav, sequence_length=16000, sequence_stride=16000, batch_size=1)
+audio_slices = audio_slices.map(preprocess_mp3)
+audio_slices = audio_slices.batch(64)
+yhat = model.predict(audio_slices)
+yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
+
+from itertools import groupby
+yhat = [key for key, group in groupby(yhat)]
+calls = tf.math.reduce_sum(yhat).numpy()
+calls
+
+
+results = {}
+for file in os.listdir(os.path.join('data', 'Forest Recordings')):
+    FILEPATH = os.path.join('data','Forest Recordings', file)
+    
+    wav = load_mp3_16k_mono(FILEPATH)
+    audio_slices = tf.keras.utils.timeseries_dataset_from_array(wav, wav, sequence_length=48000, sequence_stride=48000, batch_size=1)
+    audio_slices = audio_slices.map(preprocess_mp3)
+    audio_slices = audio_slices.batch(64)
+    
+    yhat = model.predict(audio_slices)
+    
+    results[file] = yhat
+
+results
+
+class_preds = {}
+for file, logits in results.items():
+    class_preds[file] = [1 if prediction > 0.99 else 0 for prediction in logits]
+class_preds
+
+postprocessed = {}
+for file, scores in class_preds.items():
+    postprocessed[file] = tf.math.reduce_sum([key for key, group in groupby(scores)]).numpy()
+postprocessed
+
+import csv
+with open('results.csv', 'w', newline='') as f:
+    writer = csv.writer(f, delimiter=',')
+    writer.writerow(['recording', 'capuchin_calls'])
+    for key, value in postprocessed.items():
+        writer.writerow([key, value])
