afdc604ab57fff2f420a81b1e2d58d0c049f7aa7
Fabian-Vega <fabianvega15502@gmail.com>
2024-06-22 19:19:29 -0600

    Added new functions to file

--- src/DatabaseBuilder.py
+++ src/DatabaseBuilder.py
@@ -3,6 +3,19 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_io as tfio
 
+def load_wav_16k_mono(filename):
+    # Load encoded wav file
+    file_contents = tf.io.read_file(filename)
+    # Decode wav (tensors by channels) 
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels=1)
+    # Removes trailing axis
+    wav = tf.squeeze(wav, axis=-1)
+    sample_rate = tf.cast(sample_rate, dtype=tf.int64)
+    # Goes from 44100Hz to 16000hz - amplitude of the audio signal
+    wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
+    return wav
+
+
 
 POS = os.path.join('data', 'sirens_wav')
 NEG = os.path.join( 'data', 'unheard_wav')
@@ -15,8 +28,8 @@ neg_files = os.listdir(NEG)
 #print("Negative files:", neg_files)
 
 # 3.2 Create Tensorflow Datasets
-pos = tf.data.Dataset.list_files(os.path.join(POS + '\*-*.wav'))
-neg = tf.data.Dataset.list_files(os.path.join(NEG + '\*-*.wav'))
+pos = tf.data.Dataset.list_files(POS + '/*_*.wav')
+neg = tf.data.Dataset.list_files(NEG + '/*_*.wav')
 
 # 3.2 Add labels and combine positive and negative samples
 positives = tf.data.Dataset.zip((pos, tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
