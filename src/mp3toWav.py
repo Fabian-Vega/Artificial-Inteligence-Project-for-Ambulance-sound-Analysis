f858b406a312467c0cab09f374e3d9d9a841180f
MarlonMQ <105015034+MarlonMQ@users.noreply.github.com>
2024-06-22 18:52:16 -0600

    feat: Convert MP3 files to WAV format
    This code change adds a new script, `mp3toWav.py`, which converts MP3 files to WAV format. It uses the `pydub` library to perform the conversion. The script takes a folder path as input and converts all the MP3 files in that folder to WAV format, saving them in a specified output folder. The converted files are named with the same name as the original MP3 files, but with the `.wav` extension.
    
    Recent user commits:
    - Refactor file naming in fragmentation.py and rename.py
    - Merge branch 'main' of https://github.com/Fabian-Vega/Artificial-Inteligence-Project-for-Ambulance-sound-Analysis
    - feat: Update file paths and add new audio data
    - Added initial classifier code
    - Refactor folder paths for audioClassifier.py
    - Changed location of audio files
    - add new classified sound data
    - Refactor folder paths in audioClassifier.py

---
+++ src/mp3toWav.py
@@ -0,0 +1,25 @@
+from pydub import AudioSegment
+import os
+
+# Carpeta de entrada y salida
+carpeta_entrada = '../data/sirens'
+carpeta_salida = '../data/sirens_wav'
+
+# Crear la carpeta de salida si no existe
+if not os.path.exists(carpeta_salida):
+    os.makedirs(carpeta_salida)
+
+# Recorrer todos los archivos en la carpeta de entrada
+for archivo in os.listdir(carpeta_entrada):
+    if archivo.endswith('.mp3'):
+        # Obtener la ruta completa del archivo de entrada y salida
+        ruta_entrada = os.path.join(carpeta_entrada, archivo)
+        nombre_salida = archivo.replace('.mp3', '.wav')
+        ruta_salida = os.path.join(carpeta_salida, nombre_salida)
+        
+        # Convertir el archivo .mp3 a .wav
+        cancion = AudioSegment.from_mp3(ruta_entrada)
+        cancion.export(ruta_salida, format='wav')
+        
+
+print('Todos los archivos .mp3 han sido convertidos a .wav.')
